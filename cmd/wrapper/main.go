5159517fa9c98795a1fc20af66e11d08a982d6ca
SherifEldeeb <archeldeeb@gmail.com>
2019-08-21 10:11:14 +0300

    check

--- cmd/wrapper/main.go
+++ cmd/wrapper/main.go
@@ -5,18 +5,43 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/SherifEldeeb/wrapper/pkg/service"
 )
 
+func printUsage() {
+	fmt.Println(`
+##################
+# SvcWrapper 0.1 #
+##################
+twitter.com/0xdeeb
+    
+    - For windows programs to run as services, they have to be developed in a certain way to respond to 
+      certain messages from the Windows OS ... otherwise windows kills them after 30 seconds.
+    - SvcWrapper wraps "normal" (non-service) Windows executables enabling them to run as a service.
+    - It enables "installing", "removing" and "running" applications as services.
+    
+    Usage: wrapper.exe SERVICE_NAME COMMAND [ARGS]
+        - SERVICE_NAME is the name of the service
+        - COMMAND is either 'install', 'remove' or 'run'
+
+            Example 01:
+            - > wrapper.exe kolide_launcher_service install launcher.exe --host=192.168.0.10 --insecure
+                This will create a service 'kolide_launcher_service' that will execute the 'launcher.exe'
+                with the arguments that has been provided
+
+            Example 02:
+            - > wrapper.exe kolide_launcher_service remove
+                This will stop and remove 'kolide_launcher_service' service`)
+	os.Exit(1)
+}
+
 func main() {
 	var err error
 	if len(os.Args) < 3 {
-		fmt.Println("go home")
-		os.Exit(1)
+		printUsage()
 	}
 	var svcName = os.Args[1]
 	var cmd = os.Args[2]
@@ -40,11 +65,12 @@ func main() {
 		}
 		return
 	default:
-		log.Printf("invalid command %s", cmd)
+		fmt.Printf("invalid command %s", cmd)
+		printUsage()
 	}
 
 	if err != nil {
-		log.Fatalf("failed to %s %s: %v", cmd, svcName, err)
+		panic(fmt.Sprintf("failed to %s %s: %v", cmd, svcName, err))
 	}
 	return
 }
